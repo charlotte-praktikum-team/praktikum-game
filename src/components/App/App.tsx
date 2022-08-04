132ceac0a2f0c1cf65f8f4f78bd1e42c78f45816
mihalichpalich <pavlovtfspb@gmail.com>
2022-08-04 22:18:23 +0300

    #23: fix PR comments

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="main">
-        <h1 className="main__title">Мой апп.</h1>
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div className="main">
+      <h1 className="main__title">Мой апп.</h1>
+    </div>
+  );
 }
 
 export default App;
