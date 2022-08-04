9852d9a273eadb9b64be6876958f0f23c2761a22
mihalichpalich <pavlovtfspb@gmail.com>
2022-08-04 19:52:19 +0300

    #23: added typescript with config

--- webpack.common.js
+++ webpack.common.js
@@ -2,20 +2,35 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.join(__dirname, "/build"),
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.ts', '.tsx', '.js', '.json']
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        },
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+              transpileOnly: true
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(?:svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
       },
     ]
   },
