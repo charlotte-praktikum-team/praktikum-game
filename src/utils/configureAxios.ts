ec70c356205718338237606f74ded0d9a4afff80
Matyushin Danila <d.matyushin@muon.team>
2022-08-05 13:32:44 +0300

    #21: added base url

--- src/utils/configureAxios.ts
+++ src/utils/configureAxios.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
+import { PRACTICUM_ORIGIN } from './constants';
 
 export const configureAxios = () => {
+  axios.defaults.baseURL = PRACTICUM_ORIGIN;
+
   axios.interceptors.response.use((response: AxiosResponse) => {
     return response.data;
   })
