571ac40956dab24b6b7664b6541c0a1f2a05ddb7
Alyona Irisova <elaineirr.910@gmail.com>
2022-08-09 20:56:29 +0400

    #14 fix errors

--- src/components/p/p.tsx
+++ src/components/p/p.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from 'react';
+import { FC, memo } from 'react';
 import { PProps } from './types';
 
 import './p.css';
--- src/components/smallText/smallText.tsx
+++ src/components/smallText/smallText.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from 'react';
+import { FC, memo } from 'react';
 import { SmallTextProps } from './types';
 
 import './smallText.css';
