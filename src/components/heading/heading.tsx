0c6078513740cff1bbd5a51d1cff7890d0a73217
Mickilol <33375436+Mickilol@users.noreply.github.com>
2022-08-09 19:47:50 +0300

    Task 19 (#31)
    * #19: added shared ui components
    
    * #19: added icons for the app and icon component
    
    * #19: added close icon for the modal
    
    * #19: cleared up the app component
    
    * #19: added review fixes (type prop for the button)
    
    * #19: added review fixes
    
    * #19: combined headings component to one component
    
    * #19: added heading component
    
    * #19: changed div tag to createElement function in the heading
    
    * #19: removed template string for type prop in the heading
    
    Co-authored-by: Matyushin Danila <d.matyushin@muon.team>

---
+++ src/components/heading/heading.tsx
@@ -0,0 +1,15 @@
+import { createElement, FC, memo } from 'react';
+import cn from 'classnames';
+import { HeadingProps } from './types';
+
+import './heading.css';
+
+export const Heading: FC<HeadingProps> = memo(({ children, type, size, align = 'center' }) =>
+  createElement(
+    type,
+    {
+      className: cn('heading', `heading_align-${align}`, `heading_size-${size}`),
+    },
+    children
+  )
+);
