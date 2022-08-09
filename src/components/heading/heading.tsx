47b0083da3a56ecffcc842537aff6619b74d258c
Matyushin Danila <d.matyushin@muon.team>
2022-08-09 19:43:37 +0300

    #19: changed div tag to createElement function in the heading

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -1,9 +1,15 @@
-import React, { FC, memo } from 'react';
+import { createElement, FC, memo } from 'react';
 import cn from 'classnames';
 import { HeadingProps } from './types';
 
 import './heading.css';
 
-export const Heading: FC<HeadingProps> = memo(({ children, type, align = 'center' }) => (
-  <div className={cn('heading', `heading_align-${align}`, `heading_type-${type}`)}>{children}</div>
-));
+export const Heading: FC<HeadingProps> = memo(({ children, type, size, align = 'center' }) =>
+  createElement(
+    `${type}`,
+    {
+      className: cn('heading', `heading_align-${align}`, `heading_size-${size}`),
+    },
+    children
+  )
+);
--- src/components/heading/types.ts
+++ src/components/heading/types.ts
@@ -1,8 +1,10 @@
 type Align = 'left' | 'center';
 type Type = 'h1' | 'h2' | 'h3';
+type Size = 'm' | 'l' | 'xl';
 
 export type HeadingProps = {
   children: React.ReactNode;
   align?: Align;
   type: Type;
+  size: Size;
 };
