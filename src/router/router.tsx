f0dc43d115992b71a33dae297c72b5e1dcf7b431
Alyona <75016726+elaineir@users.noreply.github.com>
2022-08-12 13:48:24 +0400

    Task 6 (#37)
    * #6 added register page
    
    * #6 updated validatonSchema type
    
    * #6 added slide-in animation
    
    * #6 update inputs for register page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,12 +6,13 @@ import { RequireAuth } from '@/router/requireAuth';
 import { routes } from '@/router/routes';
 
 const Login = lazy(() => import('@/pages/login/login'));
+const Register = lazy(() => import('@/pages/register/register'));
 
 export const Router = () => (
   <Routes>
     <Route element={<CenteredLayout />}>
       <Route path={routes.login.path} element={<Login />} />
-      <Route path={routes.register.path} element={<h1>Регистрация</h1>} />
+      <Route path={routes.register.path} element={<Register />} />
       <Route path={routes.notFound.path} element={<h1>404</h1>} />
       <Route path={routes.error.path} element={<h1>500</h1>} />
     </Route>
