e8de0e1c620adb351c0f4b775965f1ee60168091
Matyushin Danila <d.matyushin@muon.team>
2022-08-12 16:25:46 +0300

    #12: fixed review comments

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -3,60 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { P } from '@/components';
 import { LevelOverview } from './components/levelOverview';
 import { ACTIVE_LEVEL_NUMBER } from '@/utils/constants';
+import { LEVELS_STUB } from './mock';
 import './dashboard.css';
-
-const LEVELS_STUB = [
-  {
-    number: 1,
-    isComplete: true,
-    points: 2048,
-  },
-  {
-    number: 2,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 3,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 4,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 5,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 6,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 7,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 8,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 9,
-    isComplete: false,
-    points: null,
-  },
-  {
-    number: 10,
-    isComplete: false,
-    points: null,
-  },
-];
 
 const Dashboard = () => {
   const navigate = useNavigate();
