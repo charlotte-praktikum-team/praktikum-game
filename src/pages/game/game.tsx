ee8ceb64cd6fbb7be3616367b43e58c6191dddfd
Mickilol <33375436+Mickilol@users.noreply.github.com>
2022-08-14 14:07:54 +0300

    Task 15 (#42)
    * #12: added dashboard page
    
    * #12: fixed review comments
    
    * #12: fixed review comments
    
    * #12: fixed review comments
    
    * #40: added gameHeader component
    
    * Added useGameLoop hook
    
    * Moved Game component from BaseLayout
    
    * #15: added flasks displaying on canvas
    
    * #15: added ball displaying in the flasks
    
    * #15: changed flask and ball styles
    
    * #15: fixed review comments
    
    Co-authored-by: Matyushin Danila <d.matyushin@muon.team>

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,30 +1,74 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useStopwatch } from 'react-timer-hook';
 import { useNavigate } from 'react-router-dom';
 import { GameHeader } from './components/gameHeader/gameHeader';
+import { useGameLoop } from './hooks/useGameLoop';
+import { Flask } from './gameEntities/flask';
+import { initLevel } from './utils/initLevel';
+import { gameConfig } from './utils/config';
+import { routes } from '@/router/routes';
+import { ACTIVE_LEVEL_NUMBER } from '@/utils/constants';
+
+import './game.css';
 
 const Game = () => {
   const navigate = useNavigate();
-  const { minutes, seconds, reset } = useStopwatch({ autoStart: true });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const level = localStorage.getItem(ACTIVE_LEVEL_NUMBER);
 
+  const [flaskList, setFlaskList] = useState<Flask[]>(initLevel(level));
+  const [isNewFlaskAdded, setIsNewFlaskAdded] = useState<boolean>(false);
+
+  const { minutes, seconds, reset } = useStopwatch({ autoStart: true });
   const timer = `${minutes < 10 ? 0 : ''}${minutes}:${seconds < 10 ? 0 : ''}${seconds}`;
+
+  useEffect(() => {
+    if (!level) {
+      navigate(routes.game.path);
+    }
+  }, [level]);
 
   const handleRefresh = () => {
     reset();
+    setFlaskList(initLevel(level));
   };
 
   const handleBack = () => {
-    navigate(-1);
+    navigate(routes.game.path);
   };
 
   const handleAddFlask = () => {
-    // add flask
+    if (isNewFlaskAdded) {
+      return;
+    }
+
+    const lastFlask = flaskList[flaskList.length - 1];
+    const emptyFlask = new Flask(lastFlask.x + gameConfig.flask.width + gameConfig.grid.flasksGap, lastFlask.y);
+
+    setIsNewFlaskAdded(true);
+    setFlaskList([...flaskList, emptyFlask]);
   };
+
+  const render = useCallback(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext('2d');
+
+    if (canvas && ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      flaskList.forEach((flask) => flask.render(ctx));
+    }
+  }, [flaskList]);
+
+  useGameLoop(render);
 
   return (
     <>
       <GameHeader timer={timer} onBack={handleBack} onRefresh={handleRefresh} onAddFlask={handleAddFlask} />
 
-      <main>игра</main>
+      <main className='game__wrapper'>
+        <canvas width='800' height='700' ref={canvasRef} />
+      </main>
     </>
   );
 };
