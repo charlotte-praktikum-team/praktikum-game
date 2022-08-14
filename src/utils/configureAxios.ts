43aa039a1370e2c4a87214d9b381a153e3ed1246
Alyona Irisova <elaineirr.910@gmail.com>
2022-08-14 18:55:37 +0400

    #5 added AuthService and User type

--- src/utils/configureAxios.ts
+++ src/utils/configureAxios.ts
@@ -3,6 +3,7 @@ import { PRACTICUM_ORIGIN } from './constants';
 
 export const configureAxios = () => {
   axios.defaults.baseURL = PRACTICUM_ORIGIN;
+  axios.defaults.withCredentials = true;
 
   axios.interceptors.response.use((response: AxiosResponse) => response.data);
 };
