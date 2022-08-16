ca455d133352f13f320a0e5d07ac25adb8f9a40b
Alyona Irisova <elaineirr.910@gmail.com>
2022-08-16 16:02:14 +0400

    #44 fix types

--- src/store/user/selectors.ts
+++ src/store/user/selectors.ts
@@ -1,4 +1,4 @@
-import { UserState } from '@/store/user/types';
+import { UserState } from './types';
 
 export const selectUser = (state: UserState) => state.user;
 export const selectIsAuth = (state: UserState) => state.isAuth;
