711574047ee179b0aedea9667a8c9f822ccaca44
Alyona Irisova <elaineirr.910@gmail.com>
2022-08-16 18:20:54 +0400

    #44 added handleLogout to mainHeader

--- src/pages/components/mainHeader/mainHeader.tsx
+++ src/pages/components/mainHeader/mainHeader.tsx
@@ -5,10 +5,13 @@ import { Heading, Icon } from '@/components';
 import { Header } from '../header/header';
 
 import './mainHeader.css';
+import { useAuth } from '@/hooks/useAuth';
 import { routes } from '@/router/routes';
 
 export const MainHeader = memo(() => {
   const headerRoutes = [routes.game, routes.leaderboard, routes.forum, routes.profile];
+
+  const { handleLogout } = useAuth();
 
   return (
     <Header classes='header-main'>
@@ -16,7 +19,7 @@ export const MainHeader = memo(() => {
         {routes.landing.title}
       </Heading>
       <nav className='header-main__link-wrapper'>{Children.toArray(headerRoutes.map((route) => <HeaderLink routeData={route} />))}</nav>
-      <button className='header-main__exit-button'>
+      <button className='header-main__exit-button' onClick={handleLogout}>
         <Icon name='logout' />
       </button>
     </Header>
