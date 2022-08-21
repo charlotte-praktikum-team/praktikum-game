e316281c71d10b39ff4fd5d3bd917dba1c3b10c8
Matyushin Danila <d.matyushin@muon.team>
2022-08-21 20:51:02 +0300

    #51: added useFullScreen hook

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
 import { Children, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { P } from '@/components';
+import { P, SmallText } from '@/components';
 import { LevelOverview } from './components/levelOverview';
 import { ACTIVE_LEVEL_NUMBER } from '@/utils/constants';
 import { LEVELS_STUB } from './mock';
@@ -17,12 +17,18 @@ const Dashboard = () => {
 
   return (
     <div className='dashboard__wrapper'>
-      <P classes='dashboard__description'>
-        Ball Sort Puzzle - игра-головоломка, которая подойдет для тренировки мозга и поможет скоротать время. Цель игры - как можно быстрее
-        отсортировать разноцветные шарики в пробирках. Перемещать шарики можно в том случае, если они имеют один цвет и в пробирке
-        достаточно места. Если вы застряли, то можно воспользоваться одной дополнительной пробиркой, но, в этом случае, вы получите меньше
-        очков.
-      </P>
+      <div className='dashboard__description'>
+        <P>
+          Ball Sort Puzzle - игра-головоломка, которая подойдет для тренировки мозга и поможет скоротать время. Цель игры - как можно
+          быстрее отсортировать разноцветные шарики в пробирках. Перемещать шарики можно в том случае, если они имеют один цвет и в пробирке
+          достаточно места. Если вы застряли, то можно воспользоваться одной дополнительной пробиркой, но, в этом случае, вы получите меньше
+          очков.
+        </P>
+
+        <SmallText classes='dashboard__hint'>
+          Чтобы войти в полноэкранный режим, нажмите &quot;F&quot;. Чтобы выйти - &quot;F&quot; или &quot;Esc&quot;.
+        </SmallText>
+      </div>
 
       <div className='dashboard__levels-wrapper'>
         {Children.toArray(
