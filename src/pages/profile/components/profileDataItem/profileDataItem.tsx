ad5640c7fadc207adac98ac7d29a04ef2de3ea1b
mihalichpalich <pavlovtfspb@gmail.com>
2022-08-26 00:26:04 +0300

    #11: fix eslint errors

--- src/pages/profile/components/profileDataItem/profileDataItem.tsx
+++ src/pages/profile/components/profileDataItem/profileDataItem.tsx
@@ -5,13 +5,11 @@ import { ProfileListItem } from '@/pages/components/profileListItem/profileListI
 import './profileDataItem.css';
 import { ProfileDataItemProps } from './types';
 
-export const ProfileDataItem: FC<ProfileDataItemProps> = ({ title, text }) => {
-  return (
-    <ProfileListItem>
-      <div className='profile-data-item'>
-        <span>{title}</span>
-        <span className='profile-data-item__info'>{text}</span>
-      </div>
-    </ProfileListItem>
-  );
-};
+export const ProfileDataItem: FC<ProfileDataItemProps> = ({ title, text }) => (
+  <ProfileListItem>
+    <div className='profile-data-item'>
+      <span>{title}</span>
+      <span className='profile-data-item__info'>{text}</span>
+    </div>
+  </ProfileListItem>
+);
