5d10e1aecaac6e528e4f334a49e0c583107cf823
Alyona Irisova <elaineirr.910@gmail.com>
2022-08-28 20:39:52 +0400

    #57 added shadow

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -9,15 +9,18 @@ const ErrorPage: FC<ErrorPageProps> = ({ title, description }) => {
   const navigate = useNavigate();
 
   return (
-    <div className='error-page bounce-top'>
-      <Heading type='h1' size='xxl' classes='error-page__text'>
-        {title}
-      </Heading>
-      <P classes='error-page__text'>{description}</P>
-      <button className='error-page__button' onClick={() => navigate(routes.game.path, { replace: true })}>
-        <P classes='error-page__text'>в игру</P>
-      </button>
-    </div>
+    <>
+      <div className='error-page bounce-top'>
+        <Heading type='h1' size='xxl' classes='error-page__text'>
+          {title}
+        </Heading>
+        <P classes='error-page__text'>{description}</P>
+        <button className='error-page__button' onClick={() => navigate(routes.game.path, { replace: true })}>
+          <P classes='error-page__text'>в игру</P>
+        </button>
+      </div>
+      <div className='shadow' />
+    </>
   );
 };
 
