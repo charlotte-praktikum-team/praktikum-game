ed0f8a686784f667a99e096d3fc9c04092cf7c5e
Alyona <75016726+elaineir@users.noreply.github.com>
2022-08-31 15:04:16 +0400

    Task 57 (#63)
    * #57 add ErrorPage
    
    * #57 added shadow
    
    * #57 fixed semantics for button

---
+++ src/pages/error/error.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Heading, P } from '@/components';
+import { ErrorPageProps } from '@/pages/error/types';
+import './error.css';
+import { routes } from '@/router/routes';
+
+const ErrorPage: FC<ErrorPageProps> = ({ title, description }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className='error-page bounce-top'>
+        <Heading type='h1' size='xxl' classes='error-page__text'>
+          {title}
+        </Heading>
+        <P classes='error-page__text'>{description}</P>
+        <button className='error-page__button' onClick={() => navigate(routes.game.path, { replace: true })}>
+          в игру
+        </button>
+      </div>
+      <div className='shadow' />
+    </>
+  );
+};
+
+export default ErrorPage;
