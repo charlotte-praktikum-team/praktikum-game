4c196d6444311ff7897eaead26e489b325dcd292
Mickilol <33375436+Mickilol@users.noreply.github.com>
2022-09-19 11:06:48 +0300

    Task 68 (#75)
    #68: added oauth
    
    Co-authored-by: Matyushin Danila <d.matyushin@muon.team>

--- src/store/user/selectors.ts
+++ src/store/user/selectors.ts
@@ -4,3 +4,4 @@ export const selectUser = (state: AppState) => state.user.user;
 export const selectIsAuth = (state: AppState) => state.user.isAuth;
 export const selectIsLoading = (state: AppState) => state.user.isLoading;
 export const selectErrorMessage = (state: AppState) => state.user.errorMessage;
+export const selectServiceId = (state: AppState) => state.user.serviceId;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const PRACTICUM_ORIGIN = 'https://ya-praktikum.tech/api/v2';
 export const PRACTICUM_ORIGIN_AVATAR = 'https://ya-praktikum.tech/api/v2/resources';
+export const REDIRECT_URI = 'https://ball-sort-puzzle.herokuapp.com/';
 export const APP_ROOT_ID = 'root';
 export const ACTIVE_LEVEL_NUMBER = 'active level number';
 export const TEAM_NAME = 'charlotte';
