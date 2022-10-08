c1569bc3d0e5a555358bc329884b8d2c4ed4cbf1
mihalichpalich <pavlovtfspb@gmail.com>
2022-10-08 23:05:28 +0300

    #81: fix redirect

--- src/serverLogic/authMiddleware.ts
+++ src/serverLogic/authMiddleware.ts
@@ -1,26 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 import { PRACTICUM_ORIGIN } from '../utils/constants';
 import { routes } from '../router/routes';
+import { User } from '../types';
+import { practicumAxios } from '../utils/configureAxios';
 
 const pathArray = Object.values(routes).map((route) => route.path);
 
-export default async (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.cookie && req.headers.cookie.includes('authCookie')) {
-    await axios
-      .get(`${PRACTICUM_ORIGIN}/auth/user`, {
+const getUser = (req: Request) => {
+  try {
+    if (req.headers.cookie && req.headers.cookie.includes('authCookie')) {
+      const { data } = axios.get(`${PRACTICUM_ORIGIN}/auth/user`, {
         headers: { Cookie: req.headers.cookie },
-      })
-      .then((data) => {
-        if (pathArray.includes(req.url)) {
-          req.user = data.data;
-        } else {
-          res.status(401);
-        }
       });
-  } else {
-    res.redirect('/');
+      return data;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    return null;
   }
+};
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const user = await getUser(req);
+  req.user = user;
+
+  if (user) {
+    next();
+  } else {
+    if (!pathArray.includes(req.url)) {
+      return res.status(401).send();
+    } else if (pathArray.includes(req.url) && req.url !== '/') {
+      return res.redirect('/');
+    }
+  }
+
   next();
 };
